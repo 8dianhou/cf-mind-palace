85c65bd9b006e6ed43f1718da17fc0d0b4c0562c
jasonz <zyol@outlook.com>
2017-05-31 15:15:49 +0800

    1. add folder ./scripts 2. add scripts/bootstrap.sh to use cf-backbone 3. add scripts/README.md and instructions on using bootstrap.sh

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+# script it
+
+## bootstrap.sh: start a new project with cf-backbone
+usage:
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/8dianhou/cf-mind-palace/master/scripts/bootstrap.sh)" <branch>
+```
+options:
+- branch: a git branch of cf-backbone, default to master
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,18 @@
-echo cloning branch $1 from github.com/8dianhou/cf-backbone...
-git clone https://github.com/8dianhou/cf-backbone --branch $1 --single-branch ./temp
+if [ -z "$0" ]; then
+  target_branch="master"
+else
+  target_branch="$0"
+fi
+
+echo cloning branch $target_branch from github.com/8dianhou/cf-backbone...
+
+mkdir ./temp
+
+git clone https://github.com/8dianhou/cf-backbone --branch $target_branch --single-branch ./temp
+
 rm -rf ./temp/.git
+shopt -s dotglob
 mv ./temp/* ./
-mv ./temp/.* ./
+
 rm -r ./temp
+cnpm i && bower install
